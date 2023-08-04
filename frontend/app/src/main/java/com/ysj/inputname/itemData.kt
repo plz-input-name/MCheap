b1970f4022f11bc9485636597a412a427288b0a1
YangJJune <ysj7191@naver.com>
2023-08-04 14:25:53 +0900

    initialize of FrontEnd

---
+++ frontend/app/src/main/java/com/ysj/inputname/itemData.kt
@@ -0,0 +1,7 @@
+package com.ysj.inputname
+
+import java.sql.Timestamp
+
+data class itemData(
+    val name:String, val price:Int, val imgLink:String, val timeStamp: Int, val region:ArrayList<String>,
+    val id:String)
