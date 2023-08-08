ed0fbc6a9a741bedbca7df1f629284ca0ecdbc7e
white piano <startoflateness@gmail.com>
2023-08-08 14:34:11 +0900

    db model 구현

--- backend/db.sql
+++ backend/db.sql
@@ -7,3 +7,14 @@ SET
 
 -- move to mcheap
 USE mcheap;
+
+-- create tables
+CREATE TABLE
+  average_price (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    keyword VARCHAR(10) NOT NULL,
+    carrot INT NOT NULL,
+    thunder INT NOT NULL,
+    joongna INT NOT NULL,
+    colleted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  ) ENGINE = InnoDB;
