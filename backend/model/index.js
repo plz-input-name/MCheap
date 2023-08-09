33f6f4907101907372a628ee921a48a6d30f9a00
Roysung0625 <roy06255@gmail.com>
2023-08-09 14:46:41 +0900

    수정 요구사항 반영

--- backend/model/index.js
+++ backend/model/index.js
@@ -1,11 +1,12 @@
-import mysql from "mysql2/promise";
+const mysql = require("mysql2/promise");
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST ?? "127.0.0.1",
   user: process.env.DB_USER ?? "root",
   password: process.env.DB_USER
     ? process.env.DB_PASSWORD
     : process.env.DB_ROOT_PASSWORD,
-  database: "a_todo",
+  database: "testDB",
   dateStrings: true,
 });
+module.exports = pool;
