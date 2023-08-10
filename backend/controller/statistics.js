ed1c09c2e0f84f486d0451370d943482ead2d832
white piano <startoflateness@gmail.com>
2023-08-10 16:02:28 +0900

    refactoring, entry 정리

---
+++ backend/controller/statistics.js
@@ -0,0 +1,53 @@
+const createError = require("http-errors");
+const { pool, search } = require("../model");
+
+exports.getStatistics = async (req, res, next) => {
+  const size = req.query.size;
+  const keyword = req.params.keyword;
+  const conn = await pool.getConnection();
+
+  try {
+    const result = await search.find(conn, keyword, size);
+    return res.status(200).json(result[0]);
+  } catch (err) {
+    console.error(err);
+    // error 원인에 따른 code, message 세분화 필요
+    return next(createError(500, "Internal Server Error"));
+  } finally {
+    conn.release();
+  }
+};
+
+exports.makeStatistics = async (req, res, next) => {
+  const keyword = req.params.keyword;
+  const { carrot, thunder, joongna } = req.body;
+  const conn = await pool.getConnection();
+
+  try {
+    // 이탈값 제거 후 평균값 계산 필요
+    search.add(conn, keyword, carrot[0], thunder[0], joongna[0]);
+    return res.status(201).send();
+  } catch (err) {
+    console.error(err);
+    // error 원인에 따른 code, message 세분화 필요
+    return next(createError(500, "Internal Server Error"));
+  } finally {
+    conn.release();
+  }
+};
+
+exports.getLatestKeywords = async (req, res, next) => {
+  const size = req.query.size;
+  const conn = await pool.getConnection();
+
+  try {
+    const result = await search.latestKeywords(conn, size);
+    return res.status(200).json(result[0]);
+  } catch (err) {
+    console.error(err);
+    // error에 따른 code, message 세분화 필요
+    return next(createError(500, "Internal Server Error"));
+  } finally {
+    conn.release();
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,10 @@ const createError = require("http-errors");
 const express = require("express");
 const app = express();
 
-const statisticRouter = require("./routes/statistic");
-
 app.use(logger("common"));
 app.use(express.json());
 
-app.use("/statistics", statisticRouter);
+app.use("/statistics", require("./routes/statistics"));
 
 // catch 404
 app.use((_req, _res, next) => {
@@ -18,7 +16,9 @@ app.use((_req, _res, next) => {
 
 // error handler
 app.use((err, _req, res, _next) => {
-  return res.status(err.status).json({ error: err.message });
+  return res
+    .status(err.status ?? 500)
+    .json({ error: err.message ?? "Internal Server Error" });
 });
 
 app.listen(3000, () => console.log("express listenning on port 3000"));
