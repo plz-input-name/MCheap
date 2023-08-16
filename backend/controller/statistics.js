bf0a0b86fb54479f7e97401a9d0398999fa8cb9b
white piano <startoflateness@gmail.com>
2023-08-16 20:06:44 +0900

    temp

--- backend/controller/statistics.js
+++ backend/controller/statistics.js
@@ -8,6 +8,7 @@ exports.getStatistics = async (req, res, next) => {
 
   try {
     const result = await search.find(conn, keyword, size);
+
     return res.status(200).json(result[0]);
   } catch (err) {
     console.error(err.message);
@@ -22,6 +23,15 @@ exports.makeStatistics = async (req, res, next) => {
   const keyword = req.params.keyword;
   const { carrot, thunder, joongna } = req.body;
   const conn = await pool.getConnection();
+
+  // const result = await search.find(conn, keyword, 1);
+  // if (result[0][0]?.collected_at) {
+  //   if (
+  //     Date.now() - new Date(result[0][0].collected_at).valueOf() <
+  //     1000 * 60 * 60
+  //   )
+  //     return res.status(204).send();
+  // }
 
   const total = [...carrot, ...thunder, ...joongna].sort((a, b) => a - b);
   const q1 = total[Math.floor(total.length * 0.25)];
